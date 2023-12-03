35c9fdca01a66face5541207a0e8d77cc33125cd
Sab44 <sabri.ramadan34@gmail.com>
2023-12-03 13:04:51 +0100

    day1: simplify solution

--- Sources/Common/Util.swift
+++ Sources/Common/Util.swift
@@ -1,6 +1,3 @@
-// Packages require at least one swift file to prevent errors
-// This file is there for it
-// Feel free to add any code you wanna share accross multiple days
 
 public extension String {
     var lastIndex: String.Index {
--- Sources/Day01/Day01.swift
+++ Sources/Day01/Day01.swift
@@ -41,44 +41,41 @@ extension Day01 {
         var results: [String] = []
         
         input.forEach { line in
-            let firstDigitIndex = line.firstIndex { $0.isNumber } ?? line.endIndex
-            let firstDigitSubstring = String(line[..<firstDigitIndex])
+            var firstDigit = ""
             
-            let firstPresentStringNumbers = stringNumbers.filter({ firstDigitSubstring.contains($0) })
-            let firstDigit: String
-            if firstPresentStringNumbers.count > 0 {
-                firstDigit = findStringNumber(presentNumbers: firstPresentStringNumbers, substring: firstDigitSubstring, reverse: false)
-            } else {
-                firstDigit = String(line.first { $0.isNumber }!)
+        outerLoop: while (true) {
+            for charIndex in 1...line.count {
+                let substring = String(Array(line).prefix(upTo: charIndex))
+                if let lastChar = substring.last, lastChar.isNumber {
+                    firstDigit = String(lastChar)
+                    break outerLoop
+                }
+                if let numberAsWord = stringNumbers.first(where: { substring.range(of: $0) != nil }) {
+                    firstDigit = String(stringNumbers.firstIndex(of: numberAsWord)! + 1)
+                    break outerLoop
+                }
             }
+        }
             
-            let lastDigitIndex = line.lastIndex { $0.isNumber } ?? line.startIndex
-            let lastDigitSubstring = String(line[lastDigitIndex..<line.endIndex])
+            var lastDigit = ""
             
-            let lastPresentStringNumbers = stringNumbers.filter({ lastDigitSubstring.contains($0) })
-            let lastDigit: String
-            if lastPresentStringNumbers.count > 0 {
-                lastDigit = findStringNumber(presentNumbers: lastPresentStringNumbers, substring: lastDigitSubstring, reverse: true)
-            } else {
-                lastDigit = String(line.last { $0.isNumber }!)
+        outerLoop: while (true) {
+            for charIndex in 1...line.count {
+                let substring = String(Array(line).suffix(charIndex))
+                if let firstChar = substring.first, firstChar.isNumber {
+                    lastDigit = String(firstChar)
+                    break outerLoop
+                }
+                if let numberAsWord = stringNumbers.first(where: { substring.range(of: $0) != nil }) {
+                    lastDigit = String(stringNumbers.firstIndex(of: numberAsWord)! + 1)
+                    break outerLoop
+                }
             }
+        }
             
             results.append(firstDigit + lastDigit)
         }
         
         return results.map { Int($0)! }.sum()
-    }
-    
-    private static func findStringNumber(presentNumbers: [String], substring: String, reverse: Bool) -> String {
-        let substring = reverse ? String(substring.reversed()) : substring
-        var digitString = presentNumbers.map { reverse ? String($0.reversed()) : $0 }.sorted {
-            substring.range(of: $0)!.lowerBound < substring.range(of: $1)!.lowerBound
-        }.first!
-        
-        if reverse {
-            digitString = String(digitString.reversed())
-        }
-        
-        return String(stringNumbers.firstIndex(of: digitString)! + 1)
     }
 }
