5b63f1e9fa0743b9a90663ef93acc9507c32b64c
Sab44 <sabri.ramadan34@gmail.com>
2023-12-09 21:16:59 +0100

    day8: solve

--- Sources/Common/Extensions.swift
+++ Sources/Common/Extensions.swift
@@ -16,3 +16,26 @@ public extension Collection where Element: Numeric {
         return reduce(0, +)
     }
 }
+
+/*
+ Returns the Greatest Common Divisor of two numbers.
+ */
+public func gcd(_ x: Int, _ y: Int) -> Int {
+    var a = 0
+    var b = max(x, y)
+    var r = min(x, y)
+    
+    while r != 0 {
+        a = b
+        b = r
+        r = a % b
+    }
+    return b
+}
+
+/*
+ Returns the least common multiple of two numbers.
+ */
+public func lcm(_ x: Int, _ y: Int) -> Int {
+    return x / gcd(x, y) * y
+}
