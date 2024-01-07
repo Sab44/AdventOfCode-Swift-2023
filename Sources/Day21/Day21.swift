4c2af232b77d7c2327573a7ca70ed72226669540
Sab44 <sabri.ramadan34@gmail.com>
2024-01-07 09:38:06 +0100

    day21: solve part one

--- Sources/Day21/Day21.swift
+++ Sources/Day21/Day21.swift
@@ -10,17 +10,52 @@ import Common
 
 @main
 struct Day21: Puzzle {
-    typealias Input = String
-    typealias OutputPartOne = Never
-    typealias OutputPartTwo = Never
+    typealias Input = [[Character]]
+    typealias OutputPartOne = Int
+    typealias OutputPartTwo = Int
 }
 
 // MARK: - PART 1
 
 extension Day21 {
     static func solvePartOne(_ input: Input) async throws -> OutputPartOne {
-        // TODO: Solve part 1 :)
-        throw ExecutionError.notSolved
+        let startY = input.firstIndex { $0.contains("S") }!
+        let startX = input[startY].firstIndex(of: "S")!
+        let start = Coordinate(x: startX, y: startY)
+        
+        var currentPlots: Set<Coordinate> = [start]
+        
+        for _ in 0..<64 {
+            var nextPlots = Set<Coordinate>()
+            
+            for plot in currentPlots {
+                for neighbor in getNeighbors(input: input, position: plot) {
+                    nextPlots.insert(neighbor)
+                }
+            }
+            
+            currentPlots = nextPlots
+        }
+        
+       
+        return currentPlots.count
+    }
+    
+    private static func getNeighbors(input: Input, position: Coordinate) -> [Coordinate] {
+        var neighbors = [Coordinate]()
+        if let left = input[position.y].getOrNil(index: position.x - 1), left != "#" {
+            neighbors.append(Coordinate(x: position.x - 1, y: position.y))
+        }
+        if let top = input.getOrNil(index: position.y - 1)?[position.x], top != "#" {
+            neighbors.append(Coordinate(x: position.x, y: position.y - 1))
+        }
+        if let right = input[position.y].getOrNil(index: position.x + 1), right != "#" {
+            neighbors.append(Coordinate(x: position.x + 1, y: position.y))
+        }
+        if let bottom = input.getOrNil(index: position.y + 1)?[position.x], bottom != "#" {
+            neighbors.append(Coordinate(x: position.x, y: position.y + 1))
+        }
+        return neighbors
     }
 }
 
