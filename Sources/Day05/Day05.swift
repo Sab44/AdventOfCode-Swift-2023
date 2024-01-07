32e6e74bd5a68081a240339e03a4b41a4c57cc19
Sab44 <sabri.ramadan34@gmail.com>
2024-01-07 21:50:05 +0100

    day5: solve part two

--- Sources/Day05/Day05.swift
+++ Sources/Day05/Day05.swift
@@ -9,7 +9,7 @@ import AoC
 import Common
 
 struct Almanac: Parsable {
-    static let maps = [
+    static let mapIdentifiers = [
         "seed-to-soil",
         "soil-to-fertilizer",
         "fertilizer-to-water",
@@ -34,11 +34,11 @@ struct Almanac: Parsable {
         
         var allMaps: [[Map]] = []
         
-        for index in maps.indices {
-            let endIndex = index == maps.lastIndex ? (lines.endIndex + 1) : lines.firstIndex { $0.contains(maps[index + 1]) }!
+        for index in mapIdentifiers.indices {
+            let endIndex = index == mapIdentifiers.lastIndex ? (lines.endIndex + 1) : lines.firstIndex { $0.contains(mapIdentifiers[index + 1]) }!
             
             let currentMap = Array(lines[
-                (lines.firstIndex { $0.contains(maps[index]) }! + 1)..<(endIndex - 1)
+                (lines.firstIndex { $0.contains(mapIdentifiers[index]) }! + 1)..<(endIndex - 1)
             ]).map {
                 let components = $0.split(separator: " ")
                 return Map(sourceStart: Int(components[1])!, destinationStart: Int(components[0])!, range: Int(components[2])!)
@@ -83,7 +83,74 @@ extension Day05 {
 
 extension Day05 {
     static func solvePartTwo(_ input: Input) async throws -> OutputPartTwo {
-        // TODO: Solve part 2 :)
-        throw ExecutionError.notSolved
+        var seedRanges = [Range<Int>]()
+        for index in input.seeds.indices {
+            if index % 2 != 0 {
+                continue
+            }
+            
+            let range = (input.seeds[index] + 0)..<(input.seeds[index] + input.seeds[index + 1])
+            seedRanges.append(range)
+        }
+        
+        var currentRanges = seedRanges
+        for mapIndex in Almanac.mapIdentifiers.indices {
+            var newRanges = [Range<Int>]()
+            for range in currentRanges {
+                for newRange in resourceRangeToLocationRanges(almanac: input, mapIndex: mapIndex, sourceRange: range) {
+                    newRanges.append(newRange)
+                }
+            }
+            currentRanges = newRanges
+        }
+        
+        return currentRanges.min { $0.lowerBound < $1.lowerBound }!.lowerBound
+    }
+    
+    private static func resourceRangeToLocationRanges(almanac: Input, mapIndex: Int, sourceRange: Range<Int>) -> [Range<Int>] {
+        if mapIndex > Almanac.mapIdentifiers.lastIndex {
+            return [sourceRange]
+        }
+        
+        let possibleDestinations = almanac.maps[mapIndex].map {
+            let rangeShift = $0.destinationStart - $0.sourceStart
+            return ($0.sourceStart..<($0.sourceStart + $0.range), rangeShift)
+        }.sorted {
+            $0.0.lowerBound < $1.0.lowerBound
+        }
+        
+        var destinationRanges = [Range<Int>]()
+        
+        // If the sourceRange starts before the first possibleDestinationRange,
+        // then we need to add that non-overlapping part to the destinations as-is
+        let (firstPossibleDestinationRange, _) = possibleDestinations.first!
+        if sourceRange.lowerBound < firstPossibleDestinationRange.lowerBound {
+            let prefixRangeEnd = min(sourceRange.upperBound, firstPossibleDestinationRange.lowerBound)
+            let prefixRange = sourceRange.lowerBound..<prefixRangeEnd
+            destinationRanges.append(prefixRange)
+        }
+        
+        // Now, check over all the possibleDestinations and, for every destinationRange
+        // that overlaps sourceRange, apply the offset to the overlapping portion of and
+        // add it to the destinations
+        for (destinationRange, offset) in possibleDestinations {
+            if sourceRange.lowerBound < destinationRange.upperBound && destinationRange.lowerBound < sourceRange.upperBound {
+                let overlappingRangeStart = max(sourceRange.lowerBound, destinationRange.lowerBound)
+                let overlappingRangeEnd = min(sourceRange.upperBound, destinationRange.upperBound)
+                let overlappingRange = (overlappingRangeStart + offset)..<(overlappingRangeEnd + offset)
+                destinationRanges.append(overlappingRange)
+            }
+        }
+        
+        // Finally, if sourceRange extends past the end of the last destinationRange,
+        // then we need to add that non-overlapping part to the destinations as-is.
+        let (lastPossibleDestinationRange, _) = possibleDestinations.last!
+        if sourceRange.upperBound > lastPossibleDestinationRange.upperBound {
+            let suffixRangeStart = max(sourceRange.lowerBound, lastPossibleDestinationRange.upperBound)
+            let suffixRange = suffixRangeStart..<sourceRange.upperBound
+            destinationRanges.append(suffixRange)
+        }
+        
+        return destinationRanges
     }
 }
