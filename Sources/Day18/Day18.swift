7f026ec31ae79c39219da176dd65276859e00e74
Sab44 <sabri.ramadan34@gmail.com>
2024-01-07 09:33:55 +0100

    day18: solve part one

--- Sources/Day18/Day18.swift
+++ Sources/Day18/Day18.swift
@@ -8,19 +8,98 @@ import Foundation
 import AoC
 import Common
 
+struct DigInstruction: Parsable {
+    let direction: Direction
+    let meters: Int
+    let color: String
+    
+    static func parse(raw: String) throws -> DigInstruction {
+        var direction = Direction.left
+        switch raw.split(separator: " ")[0] {
+        case "U":
+            direction = .up
+        case "R":
+            direction = .right
+        case "D":
+            direction = .down
+        default:
+            direction = .left
+        }
+        
+        let meters = Int(raw.split(separator: " ")[1])!
+        let color = raw.split(separator: " ")[2].dropFirst(2).dropLast(1)
+        
+        return DigInstruction(direction: direction, meters: meters, color: String(color))
+    }
+}
+
 @main
 struct Day18: Puzzle {
-    typealias Input = String
-    typealias OutputPartOne = Never
-    typealias OutputPartTwo = Never
+    typealias Input = [DigInstruction]
+    typealias OutputPartOne = Int
+    typealias OutputPartTwo = Int
 }
 
 // MARK: - PART 1
 
 extension Day18 {
     static func solvePartOne(_ input: Input) async throws -> OutputPartOne {
-        // TODO: Solve part 1 :)
-        throw ExecutionError.notSolved
+        let start = Coordinate.zero
+        var trench = [start]
+        
+        for instruction in input {
+            for _ in 1...instruction.meters {
+                let current = trench.last!
+                trench.append(current.moved(to: instruction.direction))
+            }
+        }
+        
+        let minY = trench.map { $0.y }.min()!
+        let minX = trench.map { $0.x }.min()!
+        
+        let yOffset = minY < 0 ? -minY : 0
+        let xOffset = minX < 0 ? -minX : 0
+        
+        trench = trench.map { Coordinate(x: $0.x + xOffset, y: $0.y + yOffset) }
+        
+        let maxY = trench.map { $0.y }.max()!
+        let maxX = trench.map { $0.x }.max()!
+        
+        var visualized: [[Character]] = Array(repeating: Array(repeating: ".", count: maxX + 1), count: maxY + 1)
+        for coordinate in trench {
+            visualized[coordinate.y][coordinate.x] = "#"
+        }
+        
+        // flood fill approach
+        visualized.insert(Array(repeating: ".", count: maxX + 1), at: 0)
+        visualized.append(Array(repeating: ".", count: maxX + 1))
+        
+        visualized = visualized.map { $0.inserted(newElement: ".", at: 0) }.map { $0.plus(".") }
+        
+        let floodStart = Coordinate.zero
+        var floodStack = Stack<Coordinate>()
+        floodStack.push(floodStart)
+        
+        var flooded = Set<Coordinate>()
+        
+        while !floodStack.isEmpty {
+            let current = floodStack.pop()!
+            flooded.insert(current)
+            
+            let emptyNeighbors = current.neighbors().filter {
+                visualized.indices.contains($0.y) && visualized[0].indices.contains($0.x)
+            }.filter {
+                visualized[$0.y][$0.x] == "."
+            }
+            
+            for empty in emptyNeighbors {
+                if !flooded.contains(empty) {
+                    floodStack.push(empty)
+                }
+            }
+        }
+        
+        return (visualized.count * visualized[0].count) - flooded.count
     }
 }
 
