502966cc048c159cad4064f67f01cd926df453c7
Sab44 <sabri.ramadan34@gmail.com>
2024-01-15 14:58:08 +0100

    day18: solve part two

--- Sources/Day18/Day18.swift
+++ Sources/Day18/Day18.swift
@@ -107,7 +107,70 @@ extension Day18 {
 
 extension Day18 {
     static func solvePartTwo(_ input: Input) async throws -> OutputPartTwo {
-        // TODO: Solve part 2 :)
-        throw ExecutionError.notSolved
+        let updatedInstructions = input.map {
+            let meters = Int(String($0.color.prefix(5)), radix: 16)!
+            var newDirection = Direction.left
+            switch $0.color.suffix(1) {
+            case "0":
+                newDirection = .right
+            case "1":
+                newDirection = .down
+            case "2":
+                newDirection = .left
+            default:
+                newDirection = .up
+            }
+
+            return DigInstruction(direction: newDirection, meters: meters, color: "")
+        }
+        
+        let start = Coordinate.zero
+        var trench = [start]
+        var perimeter = 0
+        
+        for instruction in updatedInstructions {
+            let current = trench.last!
+            let next = current.moved(to: instruction.direction, steps: instruction.meters)
+            trench.append(next)
+            
+            perimeter += instruction.meters
+        }
+        
+        if trench.contains(where: { $0.y < 0 || $0.x < 0 }) {
+            let minY = trench.min { $0.y < $1.y }!.y
+            let minX = trench.min { $0.x < $1.x }!.x
+            
+            trench = trench.map { Coordinate(x: $0.x - minX, y: $0.y - minY) }
+        }
+
+        // Shoelace formula
+        // due to the definition of the puzzle, all points are already ordered
+        var result = 0
+        for index in trench.indices {
+            if index == trench.indices.lastIndex {
+                continue
+            }
+            
+            result += (trench[index].y + trench[index + 1].y) * (trench[index].x - trench[index + 1].x)
+        }
+        result = (result / 2)
+        
+        // How does the thick line of the trench contribute to overall area
+        // available for lava? Consider that the infinitesimal point in space
+        // that defines the origin is the point in the exact center of the
+        // first 1x1 cube dug. The area, as calculated, is thus bound by an
+        // infinitely thin line running through the center of the trench cubes.
+        // This means that, for your average side section of the trench, half that
+        // trench cube is in the area and half of it is outside and needs to be
+        // added back in. The exception here are corner trench cubes. For the four
+        // corners, 3/4 of that cube is outside the bounded region. This means an
+        // extra 1/4 of a trench cube for each, and for four corners... It's an
+        // extra "+ 1". The inner corners (those bends and twists in the walls)
+        // don't matter because for every cube 3/4 inside the bounded area, there's
+        // a corresponding cube with only 1/4 inside the bounded area.
+        result += (perimeter / 2) + 1
+        
+        return result
     }
+
 }
